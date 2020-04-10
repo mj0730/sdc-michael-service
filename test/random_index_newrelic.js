13748fba11c19fe6bae59d9ceb1ba8aad3cc1379
Michael Johnson <hack.reactor.mj0730@gmail.com>
2020-04-10 14:18:23 -0500

    stress testing setup complete

---
+++ test/random_index_newrelic.js
@@ -0,0 +1,10 @@
+const getRandomNumber = (context, events, done) => {
+  const min = 1;
+  const max = 31000000;
+  let index = Math.floor(Math.random() * (max - min) + min);
+
+  context.vars.index = index;
+  return done();
+};
+
+module.exports = {getRandomNumber}
